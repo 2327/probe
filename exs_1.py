40f6d26c3efcc96de18446a7d24fc78d4fd11ff0
Maxim Klyuev <max@2327.ru>
2020-02-09 22:58:39 +0300

    fixes and exercise2

--- exs_1.py
+++ exs_1.py
@@ -3,42 +3,42 @@
 '''
 
 
-def multiplication(FIRSTM, SECONDARYM):
+def multiplication(first_multiplier, secondary_multiplier):
     '''
     Генератор таблицы
     '''
     columns = []
-    for i in range(1, FIRSTM + 1):
-        for j in range(1, SECONDARYM + 1):
+    for i in range(1, first_multiplier + 1):
+        for j in range(1, secondary_multiplier + 1):
             columns.append([i, j])
 
     return columns
 
 
-def gen_table(row):
+def gen_table(row_content):
     '''
     Генератор вывода таблиц
     '''
-    return (lambda x: print(f'{x[0]} x {x[1]}'))(row)
+    return (lambda x: print(f'{x[0]} x {x[1]}'))(row_content)
 
 
-FIRSTM = None
-SECONDARYM = None
+FIRST_M = None
+SECONDARY_M = None
 
 if __name__ == '__main__':
     while True:
         try:
-            FIRSTM = int(input("Введите первый множитель: "))
+            FIRST_M = int(input("Введите первый множитель: "))
             break
         except ValueError:
             print("Введите натуральное число.")
 
     while True:
         try:
-            SECONDARYM = int(input("Введите второй множитель: "))
+            SECONDARY_M = int(input("Введите второй множитель: "))
             break
         except ValueError:
             print("Введите натуральное число.")
 
-    for row in multiplication(FIRSTM, SECONDARYM):
+    for row in multiplication(FIRST_M, SECONDARY_M):
         gen_table(row)
