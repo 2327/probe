4a77c56a9785803aeb131573a43ddc4e65a40649
Maxim Klyuev <max@2327.ru>
2020-02-09 21:21:55 +0300

    fixes

--- exs_1.py
+++ exs_1.py
@@ -1,12 +1,12 @@
 
 
-def multiplication(FIRST_M, SECONDARY_M):
+def multiplication(FIRSTM, SECONDARYM):
     '''
     Генератор таблицы
     '''
     columns = []
-    for i in range(1, FIRST_M + 1):
-        for j in range(1, SECONDARY_M + 1):
+    for i in range(1, FIRSTM + 1):
+        for j in range(1, SECONDARYM + 1):
             columns.append([i, j])
 
     return columns
@@ -22,17 +22,17 @@ def gen_table(row):
 if __name__ == '__main__':
     while True:
         try:
-            FIRST_M = int(input("Введите первый множитель: "))
+            FIRSTM = int(input("Введите первый множитель: "))
             break
         except ValueError:
             print("Введите натуральное число.")
 
     while True:
         try:
-            SECONDARY_M = int(input("Введите второй множитель: "))
+            SECONDARYM = int(input("Введите второй множитель: "))
             break
         except ValueError:
             print("Введите натуральное число.")
 
-    for row in multiplication(FIRST_M, SECONDARY_M):
+    for row in multiplication(FIRSTM, SECONDARYM):
         gen_table(row)
