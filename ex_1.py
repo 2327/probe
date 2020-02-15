264f73ba64ec91d9b3d4964d9776b26d299c471f
Maxim Klyuev <max@2327.ru>
2020-02-15 23:58:04 +0300

    step 2 and step 3

--- ex_1.py
+++ ex_1.py
@@ -1,13 +1,18 @@
 
 class ItemDiscount():
     def __init__(self, x='a', y='b'):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
 
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
 
 class ItemDiscountReport(ItemDiscount):
     def get_parent_data(self):
-        print(self.x, self.y)
+        print(self.get_x(), self.get_y())
 
 probe = ItemDiscountReport()
 probe.get_parent_data()
