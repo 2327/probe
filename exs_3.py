3ebde50a16921e1d8d4b17deb19cc575fc46c783
Maxim Klyuev <max@2327.ru>
2020-02-16 15:17:26 +0300

    ex 3

--- exs_3.py
+++ exs_3.py
@@ -1,14 +1,19 @@
 """
 Разработать генератор случайных чисел. В функцию передавать начальное и конечное число генерации
-(нуль необходимо исключить). Заполнить этими данными список и словарь. Ключи словаря должны создаваться
-по шаблону: “elem_<номер_элемента>”. Вывести содержимое созданных списка и словаря.
+(нуль необходимо исключить). Заполнить этими данными список и словарь. Ключи словаря должны
+создаваться по шаблону: “elem_<номер_элемента>”. Вывести содержимое созданных списка и словаря.
 """
 import random
 
-result_list = []
-result_dictionary = {}
+RESULT_LIST = []
+RESULT_DICTIONARY = {}
+FIRST_NUMBER = 0
+SECONDARY_NUMBER = 0
 
-def generate_random_number(start_number, end_number):
+def generate_random_number(start_number=FIRST_NUMBER, end_number=SECONDARY_NUMBER):
+    """
+    Generate random number exclude null
+    """
     result = random.randint(start_number, end_number)
     return generate_random_number() if result == 0 else result
 
@@ -28,8 +33,13 @@ if __name__ == '__main__':
         except ValueError:
             print("Введите натуральное число.")
 
-    result_list.append(generate_random_number(FIRST_NUMBER, SECONDARY_NUMBER))
-    print(result_list)
-    for i in range(len(result_list)):
-        result_dictionary['elem_'] = result_list
-    print(result_dictionary)
+    for i in range(FIRST_NUMBER, SECONDARY_NUMBER):
+        RESULT_LIST.append(generate_random_number(FIRST_NUMBER, SECONDARY_NUMBER))
+
+    print(f'Result list: {RESULT_LIST}')
+
+    for i, item in enumerate(RESULT_LIST):
+        elem = f'elem_{i}'
+        RESULT_DICTIONARY[elem] = RESULT_LIST[i]
+
+    print(f'Result list: {RESULT_DICTIONARY}')
